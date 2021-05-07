d51e1e2b5163e5c0fcce495db410e5e81fc4d370
Alfredo Peña <aalfredoplz@gmail.com>
2021-05-07 11:51:51 -0400

    method search post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+    validates :title, :content, presence: true
+
+    def self.search_post(content)
+        self.all.select {|post| ( post.title.include?(content) || post.content.include?(content) ) }
+    end
 end
